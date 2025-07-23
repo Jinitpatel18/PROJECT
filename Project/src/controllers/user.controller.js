ec43b486e16b172855c9eb104b16859c3484af13
Jinitpatel18 <jinitpatel188@gmail.com>
2025-07-23 19:51:38 +0530

    Commit After add Controller and Debug some error in code

--- Project/src/controllers/user.controller.js
+++ Project/src/controllers/user.controller.js
@@ -1,7 +1,57 @@
 import {asyncHandler} from '../utility/asyncHandler.js';
+import {ApiError} from '../utility/ApiError.js';
+import User from '../models/user.model.js';
+import { uploadOnCloudinary } from '../utility/cloudinary.js';
+import { ApiResponse } from '../utility/ApiResponse.js';
 
 export const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: "Ok"
+    const {username,email,password,fullname} = req.body;
+
+    if([username,email,fullname,password].some(fields => fields?.trim() ==="")){
+        throw new ApiError(400, "All fields are required");
+    }
+
+    const existingUser = await User.findOne(
+        {
+            $or: [
+                {email},
+                {username}
+            ]
+        }
+    )
+
+    if(existingUser){
+        throw new ApiError(400, "User already exists with this email or username");
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path
+    const coverImageLocalPath = req.files?.coverImage[0]?.path
+
+    if(!avatarLocalPath){
+        throw new ApiError(400, "Avatar is required");
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+    if(!avatar){
+        throw new ApiError(500, "Avatar upload failed");
+    }
+
+    const user = await User.create({
+        fullname,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || null,
+        email,
+        username,
+        password
     })
+
+    const createdUser = await User.findById(user._id).select("-password -refreshToken");
+
+    if(!createdUser){
+        throw new ApiError(500, "User creation failed");
+    }
+
+    return res.status(201).json(new ApiResponse(201, createdUser, "User created successfully"));
 })
--- Project/src/routes/user.routes.js
+++ Project/src/routes/user.routes.js
@@ -2,5 +2,7 @@ import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
 
 const router = Router();
-router.route('/register').post(registerUser);
+router.route('/register').post(
+    upload.single({ avatar }, { coverImage }),
+    registerUser);
 export default router;
